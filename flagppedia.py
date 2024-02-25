8a3c74529df10ea87c7b7fbc324883c23bf2cb61
Filigram <130276494+Filigram@users.noreply.github.com>
2024-02-25 18:04:35 +0400

    Add getflag func

--- flagppedia.py
+++ flagppedia.py
@@ -1,14 +1,15 @@
+
 import requests
 import pycountry
 
 class FlagpediaAPI:
 
     def __init__(self) -> None:
-        self.default_url = "https://flagcdn.com/"
+        self.default_url = "https://flagcdn.com"
         self.name_to_iso = 0
         self.iso_to_name = 1
 
-    def convert(self, code, how):
+    def convert(self, code: str, how: int) -> (str | None):
         if how == self.iso_to_name:
             try:
                 language = pycountry.languages.get(alpha_2=code)
@@ -23,4 +24,12 @@ class FlagpediaAPI:
             except LookupError:
                 return None
 
+    def get_flag(self, country_code: str, resolution: tuple[str, str]) -> (bytes | None):
+        url = f"{self.default_url}/{resolution[0]}x{resolution[1]}/{country_code}.png"
+        response = requests.get(url)
+        if not response.ok:
+            return None
+        else:
+            return response.content
+        
     
